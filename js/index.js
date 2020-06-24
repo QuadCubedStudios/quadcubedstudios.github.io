912f06e45cf8e4c5de781362134fd967f1537b47
ranga <ranga@Ranganathans-MacBook-Air.local>
2020-06-24 14:43:15 +0530

    Complete Website rework

--- js/index.js
+++ js/index.js
@@ -1,13 +1,10 @@
 /**
  * Created by raghuallthetime on 19/6/15.
  */
-$(function(){
-    $(".button-collapse").sideNav();
-});
 
 $(document).ready(function(){
 
-    $('.jumbotron h1').animate({
-        paddingLeft: '0px'
+    $('.main h1').animate({
+        opacity: '1'
     }, 1000);
 })
